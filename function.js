55f68089d2981a66ff04a38d479a5d5b93e4a4d1
Ripesoft <hello@ripesoft.co>
2026-01-04 00:37:33 +0000

    Handle keyword as object with value property

--- function.js
+++ function.js
@@ -1,13 +1,18 @@
 const functions = new Map();
 
 window.function = async function (keyword, userName, userEmail) {
+  // Extract the actual keyword value if it's an object
+  const searchTerm = typeof keyword === 'object' && keyword !== null 
+    ? (keyword.value || keyword.toString()) 
+    : String(keyword);
+  
   // Build the Wikipedia API URL to search for images
   const searchUrl = new URL('https://en.wikipedia.org/w/api.php');
   
   // First, search for the page related to the keyword
   searchUrl.searchParams.append('action', 'query');
   searchUrl.searchParams.append('format', 'json');
-  searchUrl.searchParams.append('titles', keyword);
+  searchUrl.searchParams.append('titles', searchTerm);
   searchUrl.searchParams.append('prop', 'pageimages|images');
   searchUrl.searchParams.append('pithumbsize', '300');
   searchUrl.searchParams.append('origin', '*');
